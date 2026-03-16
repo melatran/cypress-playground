9b70af96f5bed5f36d295c3f803dda90d72dbab2
Melanie Tran <melaniexx02@gmail.com>
2026-03-16 11:57:26 -0600

    child elements

--- cypress/e2e/secTest.cy.js
+++ cypress/e2e/secTest.cy.js
@@ -33,7 +33,7 @@ it('Hello world 1', () => {
     cy.get('[data-cy="inputEmail1"]') //will not always be present
 })
 
-it.only('Cypress Locator Methods', () => {
+it('Cypress Locator Methods', () => {
     //Theory
     //get() - find element in the page
     //find() - find element within another element (find only child elements)
@@ -47,4 +47,28 @@ it.only('Cypress Locator Methods', () => {
     cy.contains('nb-card', 'Horizontal form').find('button') //will find the first element with this text and then find the button within that element
     cy.contains('nb-card', 'Horizontal form').contains('Sign in') //will find the first element with this text within the element with this text and click it
     cy.contains('nb-card', 'Horizontal form').get('button') //will find all buttons within the element with this text
+})
+
+it.only('Child Elements', () => {
+
+    cy.contains('nb-card', 'Using the Grid').find('.row').find('button')
+
+    cy.get('nb-card').find('nb-radio-group').contains('Option 1')
+
+    cy.get('nb-card nb-radio-group').contains('Option 1')
+    //can also use this syntax to find the element with this text within the element with this text
+    // the space between the two elements means that we are looking for the element with this text within the element with this text
+    // try to combine two child elements into a single locator
+
+    cy.get('nb-card > nb-card-body')
+
+    cy.get('nb-card > nb-card-body [placeholder="Email"]')
+    //can also combine the child element with the attribute to find the element with this attribute within the child element
+    //found 5 elements since there are 5 cards on the page and each card has an email input field
+
+    cy.get('nb-card > nb-card-body [placeholder="Jane Doe"]')
+    // when you build selectors, make them as short as possibe but as long as necessary to make them unique and stable
+    
+    cy.get('[placeholder="Jane Doe"]')
+    //since there is only one element with this placeholder, we don't need to add the parent element to make it unique
 })
